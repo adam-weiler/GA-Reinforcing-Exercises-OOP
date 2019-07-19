e1d90e70197b709bbf9b20ad2ccf2ab438c573e9
adam-weiler <adam.weiler@yahoo.ca>
2019-07-19 13:12:04 -0400

    Able to add new task to todo list.

--- todoList.py
+++ todoList.py
@@ -1,32 +1,31 @@
-class Task():
+class TodoList(): 
+    def __init__(self):
+        self.task_list = []
 
+    def add_task(self, task):
+        self.task_list.append(task)
+        return task
+        
+
+class Task():
     def __init__(self, description, due_date):
         self.description = description
         self.due_date = due_date
 
 
-
-
-class TodoList():
-    
-    def __init__(self):
-        # self = self
-        pass
-
-    def add_task(self, task):
-        print(task)
-        pass
-
-
+todo_list = TodoList()
 exercise = Task('Exercise', 'Today')
 taxes = Task('File your taxes', 'April')
 shopping = Task('Go shopping', 'Sunday')
 
-print(exercise)
-print(taxes)
-print(shopping)
-print()
+# print(f'{exercise.description} - {exercise.due_date}')
+# print(f'{taxes.description} - {taxes.due_date}')
+# print(f'{shopping.description} - {shopping.due_date}')
+# print()
 
-
-todo_list = TodoList()
 todo_list.add_task(exercise)
+todo_list.add_task(taxes)
+todo_list.add_task(shopping)
+
+for task in todo_list.task_list:
+    print(f'{task.description} - {task.due_date}')
