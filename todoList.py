8a3e15c77cf103713130a8d68826cadc4449ed7e
adam-weiler <adam.weiler@yahoo.ca>
2019-07-20 16:46:22 -0400

    Adding show_list method.

--- todoList.py
+++ todoList.py
@@ -4,7 +4,13 @@ class TodoList():
 
     def add_task(self, task):
         self.task_list.append(task)
+        print(f'\'{task.description}\' added to list!')
         return task
+
+    def show_list(self):
+        print('This is your To-Do list:')
+        for task in self.task_list:
+            print(f'-{task.description} - {task.due_date}')
         
 class Task():
     def __init__(self, description, due_date):
@@ -19,6 +25,6 @@ shopping = Task('Go shopping', 'Sunday')
 todo_list.add_task(exercise)
 todo_list.add_task(taxes)
 todo_list.add_task(shopping)
+print()
 
-for task in todo_list.task_list:
-    print(f'{task.description} - {task.due_date}')
+todo_list.show_list()
